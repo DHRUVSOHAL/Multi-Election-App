97b887470f203876a39a9ce22b2c330da2b1d2f1
Dhruv Sohal <sohaldhruv00@gmail.com>
2026-03-21 16:29:28 +0530

    21-03-26

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose');
 
-const localURL = "mongodb://127.0.0.1:27017/voting_app2";
+// 🔥 ADD THIS LINE
+require('dns').setDefaultResultOrder('ipv4first');
+
+const localURL = process.env.MongoDB_URL || "mongodb://127.0.0.1:27017/voting_app2";
 
 mongoose.connect(localURL)
   .then(() => console.log("MongoDB connected successfully"))
